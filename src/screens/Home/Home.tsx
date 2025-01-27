8027e787ba6f8c0d1113afe725e015be0f68ac8e
Iktiup-Mahmud <seyam.official96@gmail.com>
2025-01-28 00:39:30 +0600

    feat: main_structure formed

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,78 @@
+import { useRef, useState, useEffect } from "react";
+
+const Home = () => {
+    const [isDrawing, setIsDrawing] = useState(false);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+
+        if (canvas) {
+            const ctx = canvas.getContext('2d');
+            if (ctx) {
+                canvas.width = window.innerWidth;
+                canvas.height = window.innerHeight - canvas.offsetTop;
+                ctx.lineCap = 'round';
+                ctx.lineWidth = 3;
+            }
+
+        }
+        const script = document.createElement('script');
+        script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.9/MathJax.js?config=TeX-MML-AM_CHTML';
+        script.async = true;
+        document.head.appendChild(script);
+
+        script.onload = () => {
+            window.MathJax.Hub.Config({
+                tex2jax: { inlineMath: [['$', '$'], ['\\(', '\\)']] },
+            });
+        };
+
+        return () => {
+            document.head.removeChild(script);
+        };
+
+    }, []);
+
+    const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
+        const canvas = canvasRef.current;
+        if (canvas) {
+            canvas.style.background = 'black';
+            const ctx = canvas.getContext('2d');
+            if (ctx) {
+                ctx.beginPath();
+                ctx.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+                setIsDrawing(true);
+            }
+        }
+    };
+
+    const draw = (e: React.MouseEvent<HTMLCanvasElement>) => {
+        if (!isDrawing) {
+            return;
+        }
+        const canvas = canvasRef.current;
+        if (canvas) {
+            const ctx = canvas.getContext('2d');
+            if (ctx) {
+                ctx.strokeStyle = color;
+                ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+                ctx.stroke();
+            }
+        }
+    };
+
+    const stopDrawing = () => {
+        setIsDrawing(false);
+    };
+
+    return (
+        <canvas ref={canvasRef} id='canvas' className="absolute top-0 left-0 w-full h-full" onMouseUp={stopDrawing}
+            onMouseOut={stopDrawing} onMouseDown={startDrawing} onMouseMove={draw}>
+
+        </canvas>
+    );
+};
+
+export default Home;
